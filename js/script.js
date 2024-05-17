946f33a391c7a698cc7f9a74d48ac2bc6f8120df
Andrej Sokolov <99323383+CyberPanda263@users.noreply.github.com>
2024-05-17 20:11:09 +0300

    There is a basic functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".options-form");
-const examples = document.querySelector(".examplse-box");
-
+const examples = document.querySelector(".examples-list");
+const answers = document.querySelector(".answers-list");
+const print = document.querySelector(".print");
 
 const options = {
     taskType: "0",
@@ -9,78 +10,87 @@ const options = {
 };
 
 const examplesArr = [];
-const numbersArr1 = [];
-const numbersArr2 = [];
+const answersArr = [];
+const c = [];
+const firstNumbers = [];
+const secondNumbers = [];
 
 function сreatingExamples(){
-    randomNumbers();
     if(options.taskType === "Multiplication"){
         multiplication();
     }
     if(options.taskType === "Division") {
-        division()
+        division();
     }
     if(options.taskType === "Multiplication-and-Division"){
         multiplicationAndDivision();
     }
 }
 
-function randomNumbers() {
-        for(let i = 0; i < options.numbersFrom; i++){
-            numbersArr1.push(Math.floor(Math.random() * options.numbersFrom));
-            numbersArr2.push(Math.floor(Math.random() * options.numbersFrom));
-        }
-}
-
 function multiplication() {
-    examplesArr.push('<h2>Рівняння</h2>')
-    const resultsArr =[];
+    console.log(options.unknown);
     for(let i = 0; i < options.numberOfExamples; i++) {
-        const multiplier1 = numbersArr1[Math.floor(Math.random() * numbersArr1.length)];
-        const multiplier2 = numbersArr2[Math.floor(Math.random() * numbersArr2.length)];
-        const result = multiplier1 * multiplier2;
-        const number = i + 1;
-        examplesArr.push(`<p>${number}) ${multiplier1} * ${multiplier2} = </p>`);
-        resultsArr.push(`<p>${number}) ${result}</p>`);
+        const firstNumber = Math.floor(Math.random() * options.numbersFrom);
+        const secondNumber = Math.floor(Math.random() * options.numbersFrom);
+        const result = firstNumber * secondNumber;
+        if(options.unknown === "on") {
+            if(i % 2 === 0) {
+                examplesArr.push(`<li class<li class="examples-list-item">${i + 1}) x * ${secondNumber} = ${result}</li>`)
+                answersArr.push(`<li class<li class="answers-list-item">${i + 1}) x = ${firstNumber}</li>`);
+            }else {
+                examplesArr.push(`<li class<li class="examples-list-item">${i + 1}) ${firstNumber} * x = ${result}</li>`)
+                answersArr.push(`<li class<li class="answers-list-item">${i + 1}) x = ${secondNumber}</li>`);
+            }
+
+        }else {
+            examplesArr.push(`<li class<li class="examples-list-item">${i + 1}) ${firstNumber} * ${secondNumber} = </li>`)
+            answersArr.push(`<li class<li class="answers-list-item">${i + 1}) ${result}</li>`);
+        }
     }
-    examplesArr.push('<h2>Рішення</h2>');
-    const finish = examplesArr.concat(resultsArr);
-    const setexamples = `${finish.join('')}`;
-    examples.innerHTML = setexamples;
+
+    printExamples();
 };
 
 function division() {
-    examplesArr.push('<h2>Рівняння</h2>')
-    const resultsArr =[];
-    for(let i = 0; i < options.numberOfExamples; i++) {
-        let shared;
-        let divider;
-        if(options.point != "on"){
-            for(let i = 0; i < options.numbersFrom * 2; i++) {
-               const getdivider = numbersArr2[Math.floor(Math.random() * numbersArr2.length)];
-               const getshared = numbersArr1[Math.floor(Math.random() * numbersArr1.length)];
-               const result = getshared / getdivider;
-               if(Number.isInteger(result) != false) {
-                if(getdivider != 1 && getdivider != getshared && getshared != 0) {
-                    shared = getshared;
-                    divider = getdivider;
+    let firstNumber;
+    let secondNumber;
+
+    if(options.point != "on") {
+        for(let i = 0; i < options.numberOfExamples; i++) {
+            for(let i = 0; i < 2000; i++){
+                firstNumber = Math.floor(Math.random() * options.numbersFrom);
+                secondNumber = Math.floor(Math.random() * options.numbersFrom);
+                const result = firstNumber % secondNumber;
+                const devRes = firstNumber / secondNumber;
+                if(result === 0 && firstNumber != 0 && secondNumber != 1 && devRes != 1) {
+                    firstNumbers.push(firstNumber);
+                    secondNumbers.push(secondNumber);
                     break;
                 }
-               }
             }
-        }else {
-            shared = numbersArr2[Math.floor(Math.random() * numbersArr2.length)];
-            divider = numbersArr1[Math.floor(Math.random() * numbersArr1.length)];
         }
-        const result = shared / divider;
-        const number = i + 1;
-        examplesArr.push(`<p>${number}) ${shared} : ${divider} = </p>`);
-        resultsArr.push(`<p>${number}) ${result}</p>`);
+    }else {
+        firstNumbers.push(Math.floor(Math.random() * options.numbersFrom));
+        secondNumbers.push(Math.floor(Math.random() * options.numbersFrom));
     }
-    examplesArr.push('<h2>Рішення</h2>');
-    const finish = examplesArr.concat(resultsArr);
-    const setexamples = `${finish.join('')}`;
-    examples.innerHTML = setexamples;
+    
+    for(let i = 0; i < options.numberOfExamples; i++) {
+        const result = firstNumbers[i] / secondNumbers[i];
+        if(options.unknown === "on") {
+            if(i % 2 === 0) {
+                examplesArr.push(`<li class<li class="examples-list-item">${i + 1}) x : ${secondNumbers[i]} = ${result}</li>`)
+                answersArr.push(`<li class<li class="answers-list-item">${i + 1}) x = ${firstNumbers[i]}</li>`);
+            }else {
+                examplesArr.push(`<li class<li class="examples-list-item">${i + 1}) ${firstNumbers[i]} : x = ${result}</li>`)
+                answersArr.push(`<li class<li class="answers-list-item">${i + 1}) x = ${secondNumbers[i]}</li>`);
+            }
+
+        }else {
+            examplesArr.push(`<li class<li class="examples-list-item">${i + 1}) ${firstNumbers[i]} : ${secondNumbers[i]} = </li>`)
+            answersArr.push(`<li class<li class="answers-list-item">${i + 1}) ${result}</li>`);
+        }
+    }
+    printExamples();
 };
 
 function multiplicationAndDivision() {
@@ -89,6 +99,13 @@ function multiplicationAndDivision() {
     options.numberOfExamples = roundSetNumberOfExamples;
     multiplication();
     division();
+}
+
+function printExamples() {
+    const setExamples = `${examplesArr.join("")}`;
+    const result = `${answersArr.join("")}`;
+    examples.innerHTML = setExamples;
+    answers.innerHTML = result;
 }
 
 form.addEventListener("input", event => {
@@ -106,6 +123,10 @@ form.addEventListener("input", event => {
         options.point = event.target.value;
     }
 
+    if(type === "unknown") {
+        options.unknown = event.target.value;
+    }
+
     if(type === "select-numbers") {
         options.numbersFrom = event.target.value;
     }
@@ -113,8 +134,25 @@ form.addEventListener("input", event => {
 
 form.addEventListener("submit", event => {
     event.preventDefault();
+
+    examples.innerHTML = "";
+    answers.innerHTML = "";
+    examplesArr.splice(0);
+    answersArr.splice(0);
+    firstNumbers.splice(0);
+    secondNumbers.splice(0);
+
     if(options.taskType === "0") {
         window.alert("Оберіть тип завдань");
     }
     сreatingExamples();
+})
+
+print.addEventListener("click", event => {
+    event.preventDefault();
+    form.style.display = "none";
+    print.style.display = "none";
+    window.print();
+    form.style.display = "block";
+    print.style.display = "block";
 })
